bcee66cd28a7f5a107c01d246ca9cdbec0a6e848
BenKorea <benkorea.ai@gmail.com>
2025-09-07 10:10:43 +0900

    fixed windows path

--- scripts/setup/setup_env.py
+++ scripts/setup/setup_env.py
@@ -1,7 +1,9 @@
 # scripts/setup/setup_env.py
-
 # .env.example을 복사하여 .env 생성 후, 프로젝트 폴더명과 경로 자동 치환
 # _environment 파일까지 생성
+# last modified: 2025-09-07
+# 경로의 \ -> \\ 치환 추가
+
 import shutil
 import re
 from pathlib import Path
@@ -25,7 +27,7 @@ shutil.copyfile(src, dst)
 content = dst.read_text(encoding='utf-8')
 content = re.sub(r'^PROJECT_NAME=.*', f'PROJECT_NAME={CUR_DIR_NAME}', content, flags=re.MULTILINE)
 abs_path = str(ROOT)
-content = re.sub(r'^PROJECT_ROOT=.*', f'PROJECT_ROOT={abs_path}', content, flags=re.MULTILINE)
+content = re.sub(r'^PROJECT_ROOT=.*', f'PROJECT_ROOT={abs_path.replace("\\", r"\\\\")}', content, flags=re.MULTILINE)
 content = content.replace('/home/ben/projects/rpy-quarto-template', abs_path)
 
 if os_name == "Linux":
@@ -36,7 +38,7 @@ elif os_name == "Windows":
     log_path = "%USERPROFILE%\\AppData\\Local\\{PROJECT_NAME}"
 else:
     log_path = "/var/log/{PROJECT_NAME}"
-content = re.sub(r'^LOG_PATH=.*', f'LOG_PATH={log_path}', content, flags=re.MULTILINE)
+content = re.sub(r'^LOG_PATH=.*', f'LOG_PATH={log_path.replace("\\", r"\\\\")}', content, flags=re.MULTILINE)
 
 dst.write_text(content, encoding='utf-8')
 print("[INFO] .env 파일이 생성되고, 경로 및 프로젝트명이 현재 폴더명으로 자동 치환되었습니다.")
